03f7b208552455003d6f90aab689fa4690d04a7b
Ghanshyam Gaikwad <39769182+gshyamg8@users.noreply.github.com>
2022-11-04 23:42:21 +0530

    Add files via upload

---
+++ AccessModifiers.java
@@ -0,0 +1,37 @@
+package FirstProgram;
+
+/*
+ * Final
+ * Abstract
+ * 
+ * Private   : Specific Class
+ * Public    : Any Class or Package
+ * Protected : same package OR  In Subsiding Class( with same or different package)
+ * 
+ * 
+ * DIFFERENCE between default and protected : 
+ * We c
+ * */
+
+/*
+ * Only modifiers you can use with classes are : 
+ * PUBLIC, FINAL, ABSTRACT
+ * 
+ * But,
+ * If you have Inner class then we can use PRIVATE. 
+ * 
+ * 
+ * */
+
+/*If you want to access class outside package make it PUBLIC*/
+
+/*
+ * If you don't mention access modifier for Method, Variable or Class by default 
+ * it will be DEFAULT 
+ * */
+
+public class AccessModifiers {
+	public static void main(String[] args) {
+		
+	}
+}
