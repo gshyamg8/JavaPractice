874426579558ada75297d0e8de6d5d7891022454
Ghanshyam Gaikwad <39769182+gshyamg8@users.noreply.github.com>
2022-11-06 22:23:50 +0530

    Add files via upload

---
+++ ScannerDemo.java
@@ -0,0 +1,13 @@
+package JavaPractice;
+import java.util.Scanner;
+
+public class ScannerDemo {
+	public static void main(String[] args) {
+		
+		int n = 0;
+		System.out.println("ENter a number of your choice : ");
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		System.out.println("Desired number is : " + n);
+;	}
+}
